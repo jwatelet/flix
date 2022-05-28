2005207403ea0bb68a56bff33cd9b9bf083c1128
Johan Watelet <johanwatelet@gmail.com>
2022-05-28 23:25:40 +0200

    step 20, the flash

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movies = Movie.released
   end
 
   def show
@@ -14,10 +14,37 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params[:id])
 
-    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    if @movie.update(movie_params)
+      redirect_to @movie, notice: 'Movie successfully updated!'
+    else
+      render :edit
+    end
+  end
 
-    @movie.update(movie_params)
+  def new
+    @movie = Movie.new
+  end
 
-    redirect_to @movie
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie, notice: 'Movie successfully created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+
+    redirect_to movies_url, danger: "I'm sorry, Dave, I'm afraid I can't do that!"
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :director, :duration,
+                                  :image_file_name)
   end
 end
