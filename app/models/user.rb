bd6fc0b98f6c032caef39e1ac3ea07500ae056f8
Johan Watelet <johanwatelet@gmail.com>
2022-11-02 23:37:14 +0100

    create new index and show for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
+
+  def gravatar_id
+    Digest::MD5.hexdigest(email.downcase)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   end
 
   resources :users
+  get 'signup' => 'users#new'
 end
