82996e3db519f10b50234993011d1882add8529e
Johan Watelet <johanwatelet@gmail.com>
2022-11-04 23:48:05 +0100

    add user to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
+  validates :username, presence: true,
+                       format: { with: /\A[A-Z0-9]+\z/i },
+                       uniqueness: { case_sensitive: false }
 
   def gravatar_id
     Digest::MD5.hexdigest(email.downcase)
