c68493f6c9dba96410aadea8edb11c973b16134a
Johan Watelet <johanwatelet@gmail.com>
2022-11-05 15:48:04 +0100

    add signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,5 +13,9 @@ class SessionsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, status: :see_other,
+                           notice: "You're now signed out!"
+  end
 end
