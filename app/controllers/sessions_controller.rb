bdc6f45245fc9450324c99b89a6f14dc099b9580
Johan Watelet <johanwatelet@gmail.com>
2022-11-05 14:02:56 +0100

    create session with signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,15 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    raise
+    user = User.find_by(email: params[:email_or_username]) ||
+           User.find_by(username: params[:email_or_username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = 'Invalid email/password combination!'
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy; end
