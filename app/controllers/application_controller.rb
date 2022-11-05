326a7a59f97bec22d1a1976076defe8341872172
Johan Watelet <johanwatelet@gmail.com>
2022-11-05 16:36:56 +0100

    add and update method for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   private
 
   def require_signin
+    session[:intended_url] = request.url
     redirect_to new_session_url, alert: 'Please sign in first!' unless current_user
   end
 
@@ -9,5 +10,15 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_user?(user)
+    current_user == user
+  end
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url, status: :see_other unless current_user?(@user)
+  end
+
+  helper_method :current_user?
   helper_method :current_user
 end
